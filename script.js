14aec696b334643ce784094a3217ba1bd62dd57c
ArpitVadariya <arpit092vadariya@gmail.com>
2025-03-14 07:36:09 +0530

    server create with the help of HTTP module

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-const fs = require("fs");
+const http = require("http");
 
-fs.writeFile("test.txt", " I'm fine!", function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
+const server = http.createServer(function (req, res) {
+  res.end("Heelo World!");
 });
 
-fs.appendFile("test.txt", " I'm fine!", function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
-});
-
-fs.rename("test.txt", "hello.txt", function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
-});
-
-fs.copyFile("hello.txt", "./copy/test.txt", function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
-});
-
-fs.unlink("hello.txt", function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
-});
-
-fs.rmdir("./copy", { recussive: true }, function (err) {
-  if (err) console.error(err);
-  else console.log("Done");
-});
-
-fs.readFile("test.txt", "utf8", function (data, err) {
-  if (err) console.error(err);
-  else console.log(data);
-});
+server.listen(3000);
