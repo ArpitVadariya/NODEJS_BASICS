3fb81f0f5e0a83e39e49af56b73f2ccd13e0befc
ArpitVadariya <arpit092vadariya@gmail.com>
2025-03-16 16:40:15 +0530

    cookie and session middleware added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// this two middleware use for convert data in other form for backend
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // middleware run first
 app.use(function (req, res, next) {
   console.log("middleware started");
